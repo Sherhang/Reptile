1bd6f2f51ae4d4d77818bd3f9bfb79587f2de896
Sherhang <Sherhang@github.com>
2020-01-04 17:28:50 +0800

    add

--- story/English_story.py
+++ story/English_story.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat May  4 19:02:32 2019
-
 @author: Administrator
 """
 
@@ -12,60 +11,63 @@ from email.mime.text import MIMEText
 import datetime
 import time
 
+
 def getDays():
-    
-    inlove_date=datetime.datetime(2017,8,31)
-    today_date=datetime.datetime.today()
-    inlove_days=(today_date-inlove_date).days
+    inlove_date = datetime.datetime(2020, 1, 1)
+    today_date = datetime.datetime.today()
+    inlove_days = (today_date - inlove_date).days
     return str(inlove_days)
 
-def getHTMLText(url,headers):
+
+def getHTMLText(url, headers):
     try:
-        r=requests.get(url,headers=headers,timeout=30)
+        r = requests.get(url, headers=headers, timeout=30)
         r.raise_for_status()
-        r.encoding=r.apparent_encoding
-        #print(r.text)
+        r.encoding = r.apparent_encoding
+        # print(r.text)
         return r.text
-       
+
     except:
         return "爬取失败"
-    
-def parsehtml(namelist,urllist,html):
-    url='http://www.en8848.com.cn/'
-    soup=BeautifulSoup(html,'html.parser')
-    t=soup.find(attrs={'class':'ch_content'})
-    #print(t)
-    i=t.find_all('a')
-    #print(i)
+
+
+def parsehtml(namelist, urllist, html):
+    url = 'http://www.en8848.com.cn/'
+    soup = BeautifulSoup(html, 'html.parser')
+    t = soup.find(attrs={'class': 'ch_content'})
+    # print(t)
+    i = t.find_all('a')
+    # print(i)
     for link in i[1:59:2]:
-        urllist.append(url+link.get('href'))
+        urllist.append(url + link.get('href'))
         namelist.append(link.get('title'))
 
 
 def parsehtml2(html):
-    text=[]
-    soup=BeautifulSoup(html,'html.parser')
-    t=soup.find(attrs={'class':'jxa_content','id':'articlebody'})
+    text = []
+    soup = BeautifulSoup(html, 'html.parser')
+    t = soup.find(attrs={'class': 'jxa_content', 'id': 'articlebody'})
     for i in t.findAll('p'):
         text.append(i.text)
-    #print(text)
+    # print(text)
     return "\n".join(text)
 
-def sendemail(url,headers,title):
-    date_today=time.strftime("%Y-%m-%d", time.localtime())
-    msg_from='870407139@qq.com'                                 #发送方邮箱
-    passwd=''                                                   #填入发送方邮箱的授权码
-    receivers=['870407139@qq.com']                              #收件人邮箱
-                            
-    subject="Today's story from Laofei " +str(date_today)       #主题     
-    html=getHTMLText(url,headers)
-    content='Dear Xiaofei:\n    We have been in love for '+getDays()+' Days !\n\n⭐⭐⭐⭐⭐❤❤💗❤❤⭐⭐⭐⭐⭐'+parsehtml2(html)                                        #正文
+
+def sendemail(url, headers, title):
+    date_today = time.strftime("%Y-%m-%d", time.localtime())
+    msg_from = '2940563940@qq.com'  # 发送方邮箱
+    passwd = 'dozeppgiapehdeaj'  # 填入发送方邮箱的授权码
+    receivers = ['17552759310@sina.cn']  # 收件人邮箱
+
+    subject = "Today's story on " + str(date_today)  # 主题
+    html = getHTMLText(url, headers)
+    content = getDays() + 'Days !\n\n⭐⭐⭐⭐⭐❤❤💗❤❤⭐⭐⭐⭐⭐\n\n' + parsehtml2(html)  # 正文
     msg = MIMEText(content)
     msg['Subject'] = subject
     msg['From'] = msg_from
     msg['To'] = ','.join(receivers)
     try:
-        s=smtplib.SMTP_SSL("smtp.qq.com",465)                   #邮件服务器及端口号
+        s = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 邮件服务器及端口号
         s.login(msg_from, passwd)
         s.sendmail(msg_from, msg['To'].split(','), msg.as_string())
         print("发送成功")
@@ -73,28 +75,29 @@ def sendemail(url,headers,title):
         print("发送失败")
     finally:
         s.quit()
-        
+
+
 def main():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    }
 
-
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-               }
-
-    urllist=[]
-    namelist=[]
-    for i in range(1,21):
-        if i==1:
-            url='http://www.en8848.com.cn/article/love/dating/index.html'
+    urllist = []
+    namelist = []
+    for i in range(1, 21):
+        if i == 1:
+            url = 'http://www.en8848.com.cn/article/love/dating/index.html'
         else:
-            url='http://www.en8848.com.cn/article/love/dating/index_'+str(i)+'.html'
-        print ("正在爬取第%s页的英语短文链接：" % (i))
-        print (url+'\n')
-        html=getHTMLText(url,headers)
-        parsehtml(namelist,urllist,html)
+            url = 'http://www.en8848.com.cn/article/love/dating/index_' + str(i) + '.html'
+        print("正在爬取第%s页的英语短文链接：" % (i))
+        print(url + '\n')
+        html = getHTMLText(url, headers)
+        parsehtml(namelist, urllist, html)
     print("爬取链接完成")
-    date=int(getDays())-611
-    sendemail(urllist[date],headers,namelist[date])
-    
-    
-if __name__=='__main__':
+    date = int(getDays())
+    print(date)
+    sendemail(urllist[date], headers, namelist[date])
+
+
+if __name__ == '__main__':
     main()
---
+++ story/getStory.py
@@ -0,0 +1,93 @@
+#!/home/yehang/anaconda3/envs/Spider/bin/python3
+# -*- coding: utf-8 -*-
+"""
+爬取文章写入文本
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# 返回url 的 HTML格式文本
+def getHtmlText(url, headers):
+    try:
+        r = requests.get(url, headers=headers, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding  # 自动推断类型
+        # print('r.text = ', r.text)
+        return r.text
+    except:
+        return "爬取失败"
+
+
+# 解析HTML
+# 输入参数 html 如 url = 'http://www.en8848.com.cn/article/love/dating/index.html'
+# 输出参数 namelist urllist
+def getUrlList(namelist, urllist, html='http://www.tom61.com/ertongwenxue/shuiqiangushi/index.html'):
+    url = 'http://www.tom61.com/'  # 最上层的地址
+    soup = BeautifulSoup(html, 'html.parser')
+    t = soup.find('dl', attrs={'class': 'txt_box'})  # find只找到一个满足条件的; 打开网页，查看源码
+    # print('t= ', t)
+    i = t.find_all('a')
+    print("i = ", i)
+    print("len(i) = ", len(i))
+    for link in i:  # 这里从下标1开始，步长2, 因为i 的形式是 url，name, url, name ...
+        urllist.append(url + link.get('href'))  # 文章链接
+        namelist.append(link.get('title'))  # 文章标题列表
+
+
+def getActicle(html):
+    text = []
+    soup = BeautifulSoup(html, 'html.parser')
+    t = soup.find('div', class_='t_news_txt')  # 根据class等字段得到文本
+    if t is not None:  # 防止t为None
+        # 每一个段落，加到文本里面
+        for i in t.findAll('p'):
+            text.append(i.text)
+        # print(text)
+    return "\n".join(text)
+
+
+def writeToTxt(content):
+    with open("getStoryToTxt.txt", 'w') as f:
+        f.write(content)
+
+
+def main():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) '
+                      'AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    }
+    urllist = []
+    namelist = []
+    for i in range(1, 30):  # 11在网页上查看，一共有11页, [1,22)
+        if i == 1:
+            url = 'http://www.tom61.com/ertongwenxue/shuiqiangushi/index.html'
+        else:
+            url = 'http://www.tom61.com/ertongwenxue/shuiqiangushi/index_' + str(i) + '.html'
+        print("正在爬取第%s页的英语短文链接：" % (i))
+        print(url + '\n')
+        html = getHtmlText(url, headers)
+        # print(html)
+        getUrlList(namelist, urllist, html)
+        print('len(namelist) = ', len(namelist))
+        print('len(urllist) = ', len(urllist))
+    print("爬取链接完成")
+
+    # 写入文件
+    f = open("C:/Users/EE526/Desktop/StoryInTxt.txt", 'w', encoding='utf-8')  # 覆盖写入
+    for i in range(len(urllist)):
+        html = getHtmlText(urllist[i], headers)  # 对每一个子链接解析
+        title = namelist[i]
+        if title is None:
+            title = 'unknown'
+        print('Article ' + str(i + 1))
+        content = '\n\nArticle' + str(i + 1) + ':  ' + title + '\n' + getActicle(html)  #
+        # print(content)
+        f.write(content)
+    f.close()
+    print("写入完成！")
+
+
+if __name__ == '__main__':
+    main()
