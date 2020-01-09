7a70790547827502351500489735c246c85389bc
Sherhang <Sherhang@github.com>
2020-01-09 15:24:01 +0800

    add

--- story/story.py
+++ story/story.py
@@ -37,7 +37,6 @@ def getHtmlText(url, headers):
 def getUrlList(namelist, urllist, html='http://www.tom61.com/ertongwenxue/shuiqiangushi/index.html'):
     url = 'http://www.tom61.com/'  # 最上层的地址
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup)
     t = soup.find('dl', attrs={'class': 'txt_box'})  # find只找到一个满足条件的; 打开网页，查看源码
     # print('t= ', t)
     i = t.find_all('a')
@@ -64,24 +63,28 @@ def sendemail(text):
     date_today = time.strftime("%Y-%m-%d", time.localtime())
     msg_from = '2940563940@qq.com'  # 发送方邮箱
     passwd = 'dozeppgiapehdeaj'  # 填入发送方邮箱的授权码
-    receivers = ['17552759310@sina.cn,yehang@sjtu.edu.cn']  # 收件人邮箱
+    receivers = ['17552759310@sina.cn']  # 收件人邮箱
 
     subject = "今日睡前小故事 " + str(date_today)  # 主题
 
-    content = getDays() + 'Days !\n\n⭐⭐⭐⭐⭐❤❤💗❤❤⭐⭐⭐⭐⭐\n\n' + text  # 正文
+    content = getDays() + ' Days !\n\n⭐⭐⭐⭐⭐❤❤💗❤❤⭐⭐⭐⭐⭐\n\n' + text  # 正文
     msg = MIMEText(content)
     msg['Subject'] = subject
     msg['From'] = msg_from
     msg['To'] = ','.join(receivers)
+    ret = 0
     try:
-        s = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 邮件服务器及端口号
+        s = smtplib.SMTP_SSL("smtp.qq.com",465)  # 邮件服务器及端口号
         s.login(msg_from, passwd)
         s.sendmail(msg_from, msg['To'].split(','), msg.as_string())
-        print("发送成功")
-    except:
-        print("发送失败")
-    finally:
         s.quit()
+        print("发送成功")
+        ret = 0
+    except smtplib.SMTPException as e:
+        print("发送失败: ")
+        print(e)
+        ret = 1
+    return ret
 
 
 def main():
@@ -91,7 +94,7 @@ def main():
     }
     urllist = []
     namelist = []
-    for i in range(1, 3):  # 11在网页上查看，一共有11页, [1,22)
+    for i in range(1, 30):  # 11在网页上查看，一共有11页, [1,22)
         if i == 1:
             url = 'http://www.tom61.com/ertongwenxue/shuiqiangushi/index.html'
         else:
@@ -108,24 +111,29 @@ def main():
     # 发送邮件
     day_time = int(time.mktime(datetime.date.today().timetuple()))  # 0点的时间戳
     # print(time.time()-day_time)
-    left_time = 19 * 60 * 60 + 11 * 60 - (time.time() - day_time)  # 第一个参数设置发送时间
+    left_time = 23 * 60 * 60  + 0*60 - (time.time() - day_time)  # 第一个参数设置发送时间
     # print(left_time)
     time.sleep(left_time)
-    i = 0
+    i = 2
     while 1:
         html = getHtmlText(urllist[i], headers)  # 对每一个子链接解析
         title = namelist[i]
         # if title is None:
         #     title = '无题'
-        print('Article ' + str(i + 1))
+        print('\nArticle ' + str(i + 1))
         text = '    ' + title + '\n\n' + getActicle(html)  #
         if len(text) < 20 or title is None:
+            i = i + 1
             continue  # 跳过
         print(text)
-        # sendemail(text)
+        delay = 0  # 延迟时间
+        while(sendemail(text)):
+            delay = delay + 2
+            time.sleep(2)
         i = i + 1
-        time.sleep(1 * 10 * 60)  # 间隔时间
-
+        time.sleep(24 * 60 * 60 - delay)  # 间隔时间
+        #if i > 100:
+        #  break
 
 if __name__ == '__main__':
     main()
